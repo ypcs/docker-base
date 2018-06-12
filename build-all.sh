138a65c3a1fd31af26026fceb4a4f3047f5cb772
Ville Korhonen <ville@xd.fi>
2018-06-12 13:53:39 +0300

    Drop artful

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-for suite in jessie stretch buster sid trusty xenial artful bionic
+for suite in jessie stretch buster sid xenial artful bionic
 do
     echo "I: Building \"${suite}\"..."
     sh ./build.sh "${suite}"
