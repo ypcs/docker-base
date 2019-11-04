a0944390e3203e7c48f7f9cf492d63f1f11475ef
Ville Korhonen <ville@xd.fi>
2019-11-04 16:59:53 +0200

    Skip extra sources for non-stable dists
    Some distribution versions (testing, unstable, experimental) might not
    have security/update repositories available. So, let's skip them and
    ensure our tooling "never" breaks.

--- rootfs/usr/lib/docker-helpers/build/0100-configure-sources-list.sh
+++ rootfs/usr/lib/docker-helpers/build/0100-configure-sources-list.sh
@@ -18,18 +18,28 @@ UBUNTU_MIRROR="${UBUNTU_MIRROR:-http://archive.ubuntu.com/ubuntu}"
 
 SOURCESLIST="/etc/apt/sources.list"
 
+cat /etc/debian_version
+cat /etc/os-release
+
 case "${DISTRIBUTION}"
 in
     debian)
         echo "deb ${DEBIAN_MIRROR} ${CODENAME} main" > "${SOURCESLIST}"
         echo "#deb-src ${DEBIAN_MIRROR} ${CODENAME} main" >> "${SOURCESLIST}"
-        if [ "${CODENAME}" != "sid" ]
-        then
+        case "${CODENAME}"
+	in
+	    sid|unstable)
+	        ;;
+	    bullseye|testing)
+		;;
+	    rc-buggy|experimental)
+		;;
+            *)
             echo "deb ${DEBIAN_MIRROR} ${CODENAME}-updates main" >> "${SOURCESLIST}"
             echo "#deb-src ${DEBIAN_MIRROR} ${CODENAME}-updates main" >> "${SOURCESLIST}"
             echo "deb ${DEBIAN_MIRROR}-security ${CODENAME}/updates main" >> "${SOURCESLIST}"
             echo "#deb-src ${DEBIAN_MIRROR}-security ${CODENAME}/updates main" >> "${SOURCESLIST}"
-        fi
+        esac
     ;;
     ubuntu)
 	echo "deb ${UBUNTU_MIRROR} ${CODENAME} main" > "${SOURCESLIST}"
